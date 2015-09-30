7a2ec458b72c12868a25595058c715b25fc6d361
Lope <Lope922@gmail.com>
2015-09-29 20:14:21 -0500

    added description details

--- Lab - 6 - Pizza/Form1.cs
+++ Lab - 6 - Pizza/Form1.cs
@@ -10,11 +10,17 @@ using System.Windows.Forms;
 
 namespace Lab___6___Pizza
 {
+    /* PIZZA APPLICATION THAT WILL UTILIZE COMBO BOXES RADIO BUTTONS 
+     * Pizza party calculator: include size, toppings, extras, prices, purchase summary 
+    */
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
+
+            
+
         }
     }
 }
