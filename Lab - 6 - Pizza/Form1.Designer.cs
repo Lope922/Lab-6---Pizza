fa85a53a23e2799919ddb7302e6329d4931b81ef
Lope <Lope922@gmail.com>
2015-10-01 19:25:51 -0500

    working on the toppings portion

--- Lab - 6 - Pizza/Form1.Designer.cs
+++ Lab - 6 - Pizza/Form1.Designer.cs
@@ -30,16 +30,18 @@
         {
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.radioButton1 = new System.Windows.Forms.RadioButton();
-            this.radioButton2 = new System.Windows.Forms.RadioButton();
-            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.rdoThickCrust = new System.Windows.Forms.RadioButton();
+            this.rdoThinCrust = new System.Windows.Forms.RadioButton();
+            this.rdoStuffedCrust = new System.Windows.Forms.RadioButton();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.checkBox1 = new System.Windows.Forms.CheckBox();
-            this.checkBox2 = new System.Windows.Forms.CheckBox();
-            this.checkBox3 = new System.Windows.Forms.CheckBox();
-            this.checkBox4 = new System.Windows.Forms.CheckBox();
-            this.checkBox5 = new System.Windows.Forms.CheckBox();
+            this.checkBoxGreenPepper = new System.Windows.Forms.CheckBox();
+            this.chkBoxBlackOlive = new System.Windows.Forms.CheckBox();
+            this.chkBoxSausage = new System.Windows.Forms.CheckBox();
+            this.chkBoxPepperoni = new System.Windows.Forms.CheckBox();
+            this.chkBoxCheese = new System.Windows.Forms.CheckBox();
+            this.listBoxOrderBuilder = new System.Windows.Forms.ListBox();
+            this.btnConfirm = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
@@ -47,147 +49,167 @@
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(187, 70);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBox1.Location = new System.Drawing.Point(140, 57);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(264, 219);
+            this.pictureBox1.Size = new System.Drawing.Size(198, 178);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(125, 33);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(94, 27);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(98, 16);
+            this.label1.Size = new System.Drawing.Size(80, 13);
             this.label1.TabIndex = 1;
             this.label1.Text = "Picture of Pizza";
             // 
-            // radioButton1
+            // rdoThickCrust
             // 
-            this.radioButton1.AutoSize = true;
-            this.radioButton1.Location = new System.Drawing.Point(37, 23);
-            this.radioButton1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.radioButton1.Name = "radioButton1";
-            this.radioButton1.Size = new System.Drawing.Size(95, 20);
-            this.radioButton1.TabIndex = 2;
-            this.radioButton1.TabStop = true;
-            this.radioButton1.Text = "Thick Crust";
-            this.radioButton1.UseVisualStyleBackColor = true;
+            this.rdoThickCrust.AutoSize = true;
+            this.rdoThickCrust.Location = new System.Drawing.Point(28, 19);
+            this.rdoThickCrust.Name = "rdoThickCrust";
+            this.rdoThickCrust.Size = new System.Drawing.Size(79, 17);
+            this.rdoThickCrust.TabIndex = 2;
+            this.rdoThickCrust.TabStop = true;
+            this.rdoThickCrust.Text = "Thick Crust";
+            this.rdoThickCrust.UseVisualStyleBackColor = true;
+           
             // 
-            // radioButton2
+            // rdoThinCrust
             // 
-            this.radioButton2.AutoSize = true;
-            this.radioButton2.Location = new System.Drawing.Point(37, 62);
-            this.radioButton2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.radioButton2.Name = "radioButton2";
-            this.radioButton2.Size = new System.Drawing.Size(88, 20);
-            this.radioButton2.TabIndex = 3;
-            this.radioButton2.TabStop = true;
-            this.radioButton2.Text = "Thin Crust";
-            this.radioButton2.UseVisualStyleBackColor = true;
+            this.rdoThinCrust.AutoSize = true;
+            this.rdoThinCrust.Location = new System.Drawing.Point(28, 50);
+            this.rdoThinCrust.Name = "rdoThinCrust";
+            this.rdoThinCrust.Size = new System.Drawing.Size(73, 17);
+            this.rdoThinCrust.TabIndex = 3;
+            this.rdoThinCrust.TabStop = true;
+            this.rdoThinCrust.Text = "Thin Crust";
+            this.rdoThinCrust.UseVisualStyleBackColor = true;
             // 
-            // radioButton3
+            // rdoStuffedCrust
             // 
-            this.radioButton3.AutoSize = true;
-            this.radioButton3.Location = new System.Drawing.Point(37, 106);
-            this.radioButton3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.radioButton3.Name = "radioButton3";
-            this.radioButton3.Size = new System.Drawing.Size(103, 20);
-            this.radioButton3.TabIndex = 4;
-            this.radioButton3.TabStop = true;
-            this.radioButton3.Text = "Stuffed Crust";
-            this.radioButton3.UseVisualStyleBackColor = true;
+            this.rdoStuffedCrust.AutoSize = true;
+            this.rdoStuffedCrust.Location = new System.Drawing.Point(28, 86);
+            this.rdoStuffedCrust.Name = "rdoStuffedCrust";
+            this.rdoStuffedCrust.Size = new System.Drawing.Size(86, 17);
+            this.rdoStuffedCrust.TabIndex = 4;
+            this.rdoStuffedCrust.TabStop = true;
+            this.rdoStuffedCrust.Text = "Stuffed Crust";
+            this.rdoStuffedCrust.UseVisualStyleBackColor = true;
             // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add(this.radioButton1);
-            this.groupBox1.Controls.Add(this.radioButton3);
-            this.groupBox1.Controls.Add(this.radioButton2);
-            this.groupBox1.Location = new System.Drawing.Point(53, 335);
-            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox1.Controls.Add(this.rdoThickCrust);
+            this.groupBox1.Controls.Add(this.rdoStuffedCrust);
+            this.groupBox1.Controls.Add(this.rdoThinCrust);
+            this.groupBox1.Location = new System.Drawing.Point(40, 272);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.groupBox1.Size = new System.Drawing.Size(267, 182);
+            this.groupBox1.Size = new System.Drawing.Size(200, 148);
             this.groupBox1.TabIndex = 5;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "groupBox1";
             // 
             // groupBox2
             // 
-            this.groupBox2.Controls.Add(this.checkBox5);
-            this.groupBox2.Controls.Add(this.checkBox4);
-            this.groupBox2.Controls.Add(this.checkBox3);
-            this.groupBox2.Controls.Add(this.checkBox2);
-            this.groupBox2.Controls.Add(this.checkBox1);
-            this.groupBox2.Location = new System.Drawing.Point(349, 345);
+            this.groupBox2.Controls.Add(this.checkBoxGreenPepper);
+            this.groupBox2.Controls.Add(this.chkBoxBlackOlive);
+            this.groupBox2.Controls.Add(this.chkBoxSausage);
+            this.groupBox2.Controls.Add(this.chkBoxPepperoni);
+            this.groupBox2.Controls.Add(this.chkBoxCheese);
+            this.groupBox2.Location = new System.Drawing.Point(262, 280);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(2);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(278, 148);
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(2);
+            this.groupBox2.Size = new System.Drawing.Size(208, 120);
             this.groupBox2.TabIndex = 6;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Choose your toppings";
             // 
-            // checkBox1
+            // checkBoxGreenPepper
             // 
-            this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(7, 22);
-            this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(77, 20);
-            this.checkBox1.TabIndex = 0;
-            this.checkBox1.Text = "Cheese";
-            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBoxGreenPepper.AutoSize = true;
+            this.checkBoxGreenPepper.Location = new System.Drawing.Point(88, 43);
+            this.checkBoxGreenPepper.Margin = new System.Windows.Forms.Padding(2);
+            this.checkBoxGreenPepper.Name = "checkBoxGreenPepper";
+            this.checkBoxGreenPepper.Size = new System.Drawing.Size(92, 17);
+            this.checkBoxGreenPepper.TabIndex = 4;
+            this.checkBoxGreenPepper.Text = "Green Pepper";
+            this.checkBoxGreenPepper.UseVisualStyleBackColor = true;
             // 
-            // checkBox2
+            // chkBoxBlackOlive
             // 
-            this.checkBox2.AutoSize = true;
-            this.checkBox2.Location = new System.Drawing.Point(7, 52);
-            this.checkBox2.Name = "checkBox2";
-            this.checkBox2.Size = new System.Drawing.Size(93, 20);
-            this.checkBox2.TabIndex = 1;
-            this.checkBox2.Text = "Pepperoni";
-            this.checkBox2.UseVisualStyleBackColor = true;
+            this.chkBoxBlackOlive.AutoSize = true;
+            this.chkBoxBlackOlive.Location = new System.Drawing.Point(88, 18);
+            this.chkBoxBlackOlive.Margin = new System.Windows.Forms.Padding(2);
+            this.chkBoxBlackOlive.Name = "chkBoxBlackOlive";
+            this.chkBoxBlackOlive.Size = new System.Drawing.Size(80, 17);
+            this.chkBoxBlackOlive.TabIndex = 3;
+            this.chkBoxBlackOlive.Text = "Black Olive";
+            this.chkBoxBlackOlive.UseVisualStyleBackColor = true;
             // 
-            // checkBox3
+            // chkBoxSausage
             // 
-            this.checkBox3.AutoSize = true;
-            this.checkBox3.Location = new System.Drawing.Point(7, 80);
-            this.checkBox3.Name = "checkBox3";
-            this.checkBox3.Size = new System.Drawing.Size(85, 20);
-            this.checkBox3.TabIndex = 2;
-            this.checkBox3.Text = "Sausage";
-            this.checkBox3.UseVisualStyleBackColor = true;
+            this.chkBoxSausage.AutoSize = true;
+            this.chkBoxSausage.Location = new System.Drawing.Point(5, 65);
+            this.chkBoxSausage.Margin = new System.Windows.Forms.Padding(2);
+            this.chkBoxSausage.Name = "chkBoxSausage";
+            this.chkBoxSausage.Size = new System.Drawing.Size(68, 17);
+            this.chkBoxSausage.TabIndex = 2;
+            this.chkBoxSausage.Text = "Sausage";
+            this.chkBoxSausage.UseVisualStyleBackColor = true;
             // 
-            // checkBox4
+            // chkBoxPepperoni
             // 
-            this.checkBox4.AutoSize = true;
-            this.checkBox4.Location = new System.Drawing.Point(117, 22);
-            this.checkBox4.Name = "checkBox4";
-            this.checkBox4.Size = new System.Drawing.Size(98, 20);
-            this.checkBox4.TabIndex = 3;
-            this.checkBox4.Text = "Black Olive";
-            this.checkBox4.UseVisualStyleBackColor = true;
+            this.chkBoxPepperoni.AutoSize = true;
+            this.chkBoxPepperoni.Location = new System.Drawing.Point(5, 42);
+            this.chkBoxPepperoni.Margin = new System.Windows.Forms.Padding(2);
+            this.chkBoxPepperoni.Name = "chkBoxPepperoni";
+            this.chkBoxPepperoni.Size = new System.Drawing.Size(74, 17);
+            this.chkBoxPepperoni.TabIndex = 1;
+            this.chkBoxPepperoni.Text = "Pepperoni";
+            this.chkBoxPepperoni.UseVisualStyleBackColor = true;
             // 
-            // checkBox5
+            // chkBoxCheese
             // 
-            this.checkBox5.AutoSize = true;
-            this.checkBox5.Location = new System.Drawing.Point(117, 53);
-            this.checkBox5.Name = "checkBox5";
-            this.checkBox5.Size = new System.Drawing.Size(115, 20);
-            this.checkBox5.TabIndex = 4;
-            this.checkBox5.Text = "Green Pepper";
-            this.checkBox5.UseVisualStyleBackColor = true;
+            this.chkBoxCheese.AutoSize = true;
+            this.chkBoxCheese.Location = new System.Drawing.Point(5, 18);
+            this.chkBoxCheese.Margin = new System.Windows.Forms.Padding(2);
+            this.chkBoxCheese.Name = "chkBoxCheese";
+            this.chkBoxCheese.Size = new System.Drawing.Size(62, 17);
+            this.chkBoxCheese.TabIndex = 0;
+            this.chkBoxCheese.Text = "Cheese";
+            this.chkBoxCheese.UseVisualStyleBackColor = true;
+            // 
+            // listBoxOrderBuilder
+            // 
+            this.listBoxOrderBuilder.FormattingEnabled = true;
+            this.listBoxOrderBuilder.Location = new System.Drawing.Point(491, 66);
+            this.listBoxOrderBuilder.Name = "listBoxOrderBuilder";
+            this.listBoxOrderBuilder.Size = new System.Drawing.Size(120, 95);
+            this.listBoxOrderBuilder.TabIndex = 7;
+            // 
+            // btnConfirm
+            // 
+            this.btnConfirm.Location = new System.Drawing.Point(475, 316);
+            this.btnConfirm.Name = "btnConfirm";
+            this.btnConfirm.Size = new System.Drawing.Size(75, 23);
+            this.btnConfirm.TabIndex = 8;
+            this.btnConfirm.Text = "&Confirm";
+            this.btnConfirm.UseVisualStyleBackColor = true;
+            this.btnConfirm.Click += new System.EventHandler(this.btnConfirm_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1020, 556);
+            this.ClientSize = new System.Drawing.Size(765, 452);
+            this.Controls.Add(this.btnConfirm);
+            this.Controls.Add(this.listBoxOrderBuilder);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.pictureBox1);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
@@ -204,16 +226,18 @@
 
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.RadioButton radioButton1;
-        private System.Windows.Forms.RadioButton radioButton2;
-        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton rdoThickCrust;
+        private System.Windows.Forms.RadioButton rdoThinCrust;
+        private System.Windows.Forms.RadioButton rdoStuffedCrust;
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.GroupBox groupBox2;
-        private System.Windows.Forms.CheckBox checkBox3;
-        private System.Windows.Forms.CheckBox checkBox2;
-        private System.Windows.Forms.CheckBox checkBox1;
-        private System.Windows.Forms.CheckBox checkBox5;
-        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.CheckBox chkBoxSausage;
+        private System.Windows.Forms.CheckBox chkBoxPepperoni;
+        private System.Windows.Forms.CheckBox chkBoxCheese;
+        private System.Windows.Forms.CheckBox checkBoxGreenPepper;
+        private System.Windows.Forms.CheckBox chkBoxBlackOlive;
+        private System.Windows.Forms.ListBox listBoxOrderBuilder;
+        private System.Windows.Forms.Button btnConfirm;
     }
 }
 
--- Lab - 6 - Pizza/Form1.cs
+++ Lab - 6 - Pizza/Form1.cs
@@ -10,17 +10,129 @@ using System.Windows.Forms;
 
 namespace Lab___6___Pizza
 {
-    /* PIZZA APPLICATION THAT WILL UTILIZE COMBO BOXES RADIO BUTTONS 
-     * Pizza party calculator: include size, toppings, extras, prices, purchase summary 
+    /* PIZZA APPLICATION THAT WILL UTILIZE COMBO BOXES RADIO BUTTONS TO ALLOW USER TO BUILD PIZZA ORDERS
+     * Pizza party calculator SHOUDL INCLUDE THE FOLLOWING: include size, toppings, extras, prices, purchase summary 
+     * CONOR , JIM , CARLOS 
     */
+
+    //other features will be coded later just trying to get the basics going. 
+
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
+        }
 
+
+        //BRAINSTORMING PIZZA STUCTURE FOR pizzaOrder;
+        // STRING for the type of crust 
+        string crust = "";
+        // list of the toppings selected from checkbox 
+        List<string> toppingsList = new List<string>();
+
+
+
+
+
+
+
+        private void btnConfirm_Click(object sender, EventArgs e)
+        {
+            // prompt the user to see if their selection is correct
+            DialogResult result = MessageBox.Show("Are you sure you want to place your order?", "Confirm", MessageBoxButtons.YesNo);
+
+
+            if (result == DialogResult.Yes)
+            {
+
+                // call the method to get the toppings and pass it the check boxes as well as the list to store the topping selections
+                GetToppings(chkBoxCheese, chkBoxPepperoni, chkBoxSausage, chkBoxBlackOlive, checkBoxGreenPepper, toppingsList);
+
+                
+
+                // add the crust to the listbox
+                listBoxOrderBuilder.Items.Add(GetCrustType(rdoThickCrust, rdoThinCrust, rdoStuffedCrust));
+
+                // add the toppings to the list box 
+                listBoxOrderBuilder.Items.Add(GetToppings(chkBoxCheese, chkBoxPepperoni, chkBoxSausage, chkBoxBlackOlive, checkBoxGreenPepper, toppingsList));
+
+
+                //need to find a way to add the toppings to the list and test to make sure they go to the right pizza order. 
+                // add each item in the toppings list to the list box 
+                //      foreach (string topping in toppingsList)
+                //    {
+                //      listBoxOrderBuilder.Items.Add(topping);
+                //}
+            }
+
+        }
+
+
+        public string GetCrustType(RadioButton radioBtn1, RadioButton radioBtn2, RadioButton radioBtn3)
+        {
+            string crustType = "none";
+            if (radioBtn1.Checked == true)
+            {
+                crustType = "Thick Crust";
+                return crustType;
+
+            }
+            else if (radioBtn2.Checked == true)
+            {
+                crustType = "Thin Crust";
+                return crustType;
+            }
+            else if (radioBtn3.Checked == true)
+            {
+                crustType = "Stuffed Crust";
+                return crustType;
+            }
+            else
+            {
+                return crustType;
+            }
+
+        }
+
+
+
+
+
+        // try and pull the infro from the selected toppings check boxes 
+        private List<string> GetToppings(CheckBox Cheese, CheckBox Pepperoni, CheckBox Sausage, CheckBox BlackOlives, CheckBox GreenPepper, List<string> toppingList)
+        {
+            if (Cheese.Checked == true)
+            {
+                toppingList.Add("Cheese");
+            }
+
+
+            else if (Pepperoni.Checked == true)
+            {
+                toppingList.Add("Pepperoni");
+            }
+
+            else if (Sausage.Checked == true)
+            {
+                toppingList.Add("Sausage");
+            }
             
+            else if (BlackOlives.Checked == true)
+            {
+                toppingList.Add("Black Olives");
+            }
+            
+            else if (GreenPepper.Checked == true)
+            {
+                toppingList.Add("Green Peppers");
+                return toppingsList;
+            }
 
+            else
+            {
+                return toppingsList;
+            }
         }
     }
 }
